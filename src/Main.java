f3b25b864684659874001739d205a1d1768d1220
yali <yusufali0147@hotmail.com>
2023-08-13 17:47:26 +0300

    son commit

--- src/Main.java
+++ src/Main.java
@@ -12,31 +12,33 @@ public class Main {
         kg = imput.nextDouble();
 
         double vki = ((kg / (m * m)));
-
-        System.out.println("Vücut Kitle Endeksiniz : " + vki);
+        System.out.printf("Vücut Kitle Endeksiniz : " + "%.2f\n",vki);
 
         if (vki<=15){ System.out.println("Çok ciddi derecede kilonuz düşüktür.");
         }
-        else if ((15<vki)||(vki <=16)){
+        else if (15<vki&&vki <=16){
             System.out.println("Ciddi derecede kilonuz düşüktür. ");
         }
-        else if ((16<vki)||(vki<=18.5)){
+        else if (16<vki&&vki<=18.5){
             System.out.println("Kilonuz düşüktür.");
         }
-        else if ((18.5<vki)||(vki<=25)) {
+        else if (18.5<vki&&vki<=25) {
             System.out.println("Tebrikler.");
             System.out.println("İdeal kilo aralığındasınız.");
         }
-        else if ((25<vki)||(vki<=30)) {
+        else if ((25<vki)&&(vki<=30)) {
             System.out.println("Kilonuz fazladır.");}
-        else if ((30<vki)||(vki<=35)){
+        else if ((30<vki)&&(vki<=35)){
             System.out.println("1.derecede (hafif) obezsiniz");
         }
-        else if ((35<vki)||(vki<=40)){
+        else if ((35<vki)&&(vki<=40)){
             System.out.println("2.derecede (ciddi) obezsiniz");
         }
         else if ((40<vki)){
             System.out.println("3.derecede (çok ciddi) obezsiniz");
+        }
+        else {
+            System.out.println("Hatalı bir değer girdiniz.");
         }
 
         System.out.print("Erkeklerde ideal vücut kitle indeksi 20-25 aralığıdır.\n" + "Kadınlarda ideal vücut kitle indeksi 19-24 aralığıdır.");
